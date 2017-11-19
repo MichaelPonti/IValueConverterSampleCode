cdfaddcc4f4df137f6799245aaecd01eed16374f
Michael Ponti <michael.ponti@live.ca>
2017-11-19 05:38:15 -0800

    first commit.

---
+++ Demo/Demo/EmployeeConverters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Demo
+{
+	public class EmployeeColorConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			EmployeeTypeEnum empType = (EmployeeTypeEnum) value;
+			switch(empType)
+			{
+				case EmployeeTypeEnum.CEO:
+					return Brushes.Aqua;
+				case EmployeeTypeEnum.EngDirector:
+					return Brushes.Green;
+				case EmployeeTypeEnum.Engineer:
+					return Brushes.Red;
+				case EmployeeTypeEnum.VicePres:
+					return Brushes.Blue;
+				default:
+					return Brushes.Black;
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Demo/Demo/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+	public class MainWindowViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+
+		protected bool SetProperty<T>(ref T v, T value, [CallerMemberName] string propName = null)
+		{
+			if (Equals(v, value))
+				return false;
+
+			v = value;
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+			return true;
+		}
+
+
+
+		private bool _isAccounting = false;
+		public bool IsAccounting
+		{
+			get => _isAccounting;
+			set => SetProperty<bool>(ref _isAccounting, value);
+		}
+
+
+		private string _secretAccountCode = null;
+		public string SecretAccountCode
+		{
+			get => _secretAccountCode;
+			set => SetProperty<string>(ref _secretAccountCode, value);
+		}
+	}
+}
